cf04edf51bbf07f9667129c0e09ab8e4a226ec0d
sarowarjahan <sarowar@annanovas.com>
2016-11-27 21:20:59 -0500

    initial commit

---
+++ src/hello/HelloWorld.java
@@ -0,0 +1,16 @@
+package hello;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("Hello");
+		int number1 = 10;
+		int number2 = 20;
+		
+		int total = number1 - number2;
+		System.out.println(total);
+
+	}
+
+}
